a77c2acf96ea65e29921cdc428617392f7e08450
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-12 15:47:20 -0300

    agrege la logica del programa que pide ingresar un numero y lo mutiplica por 10 y le muestra al usuario

---
+++ js/ejercios/14-PSeIntNumeroPorDiez.js
@@ -0,0 +1,4 @@
+let numero = parseInt(prompt(`Ingrese un numero para ser multiplicado por 10: `));
+let multiplicacion = numero*10;
+
+document.writeln(`La multiplicacion de el numero ${numero} por 10 es: ${multiplicacion}`);
