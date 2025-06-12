217a139c5e2d5271502dda321fb4c82e3c82d1f4
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-12 00:05:13 -0300

    hice la logica del ejercicio 6 que era ingresar dos numeros y decir cual es mayor

---
+++ js/ejercios/06-numeroMayor.js
@@ -0,0 +1,7 @@
+let num1 = parseInt(prompt("Ingrese el primer numero: ")), num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+if(num1 > num2){
+    document.writeln(`El numero ${num1} es mayor que el numero ${num2}`)
+}else{
+    document.writeln(`El numero ${num2} es mayor que el numero ${num1}`)
+}
