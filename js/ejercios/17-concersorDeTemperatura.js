79887669c095d30dc8fb6c779ba57150a012698a
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-12 16:56:52 -0300

    agrege la estructura del logaritmo para que pida ingresar una tempertura en grado Centigrado y lo convierta en grado Fahrenheit y lo muestre

---
+++ js/ejercios/17-concersorDeTemperatura.js
@@ -0,0 +1,4 @@
+let gradoCentigrado = parseInt(prompt(`Ingrese los grados centigrados: `));
+let gradoFahrenheit = ((gradoCentigrado*9)/5)+32;
+
+document.writeln(`Los grados Centigrados ${gradoCentigrado} en grados Fahrenheit son: ${gradoFahrenheit}`);
