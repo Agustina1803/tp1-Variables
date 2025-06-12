6e16e2b98076333cc52ac147cb041ca9df5283a8
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-12 14:42:10 -0300

    agrege la logica al programa para que pida dos numero, haga la su,a y le muestre al usuario

---
+++ js/ejercios/12-PSeIntSuma.js
@@ -0,0 +1,6 @@
+let numero1 = parseInt(prompt(`Ingrese un numero `)),
+  numero2 = parseInt(prompt(`Ingrese otro numero: `));
+
+let suma = numero1 + numero2; 
+
+document.writeln(`La suma de los numeros ${numero1} y ${numero2} es: ${suma}`);
